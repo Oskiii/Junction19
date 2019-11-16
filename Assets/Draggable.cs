6379771918802e61646ff659bf5ece3c822318e3
Oskari Liukku <oliukku@gmail.com>
2019-11-16 21:18:47 +0200

    Draggables

---
+++ Assets/Draggable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private Selectable _selectable;
+    private Vector3 mOffset;
+
+    private bool _allowDragging => _selectable.IsSelected;
+
+    private void Start()
+    {
+        _selectable = GetComponentInChildren<Selectable>();
+    }
+
+    private void OnMouseDown()
+    {
+        mOffset = transform.position - _selectable.GetMouseAsWorldPoint();
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = _selectable.GetMouseAsWorldPoint() + mOffset;
+    }
+}
