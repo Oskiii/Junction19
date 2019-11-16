5da7756606f68eaf993287f90a60079efac69d34
Jussi Joki <judejoki@gmail.com>
2019-11-16 13:24:39 +0200

    include extensions localy

--- Assets/Scripts/Managers/MultiplayerManager.cs
+++ Assets/Scripts/Managers/MultiplayerManager.cs
@@ -1,17 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
 
 public class MultiplayerManager : MonoBehaviour
 {
+    private NetworkManager _manager;
+    private NetworkClient _client;
+    
     // Start is called before the first frame update
     void Start()
-    {    
+    {
+        _manager = GetComponent<NetworkManager>();
+        if (_manager.matchMaker == null)
+        {
+            _manager.StartMatchMaker();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CreateGame()
     {
-        
+        _manager.matchMaker.CreateMatch(
+            _manager.matchName,
+            _manager.matchSize, 
+            true, 
+            "", 
+            "", 
+            "", 
+            0, 
+            0, 
+            _manager.OnMatchCreate);
+    }
+
+    public void JoinGame()
+    {
+        _manager.matchMaker.ListMatches(
+            0, 
+            20, 
+            "", 
+            false, 
+            0, 
+            0, 
+            OnMatchList);
+    }
+
+    private void OnMatchList(bool success, string extendedInfo, List<MatchInfoSnapshot> matchList)
+    {
+        if (LogFilter.logDebug)
+        {
+            Debug.LogFormat(
+                "NetworkManager OnMatchList Success:{0}, ExtendedInfo:{1}, matchList.Count:{2}", 
+                success, 
+                extendedInfo, 
+                matchList.Count);
+        }
+
+        _manager.matches = matchList;
+
+        if (_manager.matches.Count == 1)
+        {
+            var match = _manager.matches[0];
+            _manager.matchName = match.name;
+            _manager.matchMaker.JoinMatch(
+                match.networkId,
+                "", 
+                "", 
+                "", 
+                0, 
+                0,
+                OnMatchJoined);
+        }
+            
+    }
+
+    private void OnMatchJoined(bool success, string extendedInfo, MatchInfo matchInfo)
+    {
+        if (LogFilter.logDebug)
+        {
+            Debug.LogFormat("NetworkManager OnMatchJoined Success:{0}, ExtendedInfo:{1}, matchInfo:{2}",
+                success,
+                extendedInfo,
+                matchInfo);
+        }
+
+        if (success)
+        {
+            _client = _manager.StartClient(matchInfo);
+        }
     }
 }
